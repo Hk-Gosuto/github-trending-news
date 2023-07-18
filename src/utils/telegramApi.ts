edf6459ca4aa8a222ec1791276716b3cfaafcc6e
Hk-Gosuto <yominogosuto@gmail.com>
2023-07-18 13:05:51 +0800

    chore: 优化输出

--- src/utils/telegramApi.ts
+++ src/utils/telegramApi.ts
@@ -5,7 +5,7 @@ const telegramChannelId = process.env.TELEGRAM_CHANNEL_ID!;
 
 export async function sendMessage(text: string) {
     const url = `https://api.telegram.org/bot${telegramToken}/` + 'sendMessage';
-    const data = { chat_id: telegramChannelId, text, parse_mode: 'markdown' };
+    const data = { chat_id: telegramChannelId, text, parse_mode: 'Markdown', disable_web_page_preview: true };
     const response = await axios.post(url, data);
     return response.data;
 }
